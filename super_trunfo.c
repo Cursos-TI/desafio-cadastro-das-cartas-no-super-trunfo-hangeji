a7813046893dc4022be09c904dfe2cbd74ee25d3
hangeji <annajanampc@gmail.com>
2025-04-10 01:11:20 +0000

    commitaventureiro

--- super_trunfo.c
+++ super_trunfo.c
@@ -5,13 +5,16 @@
 
 int main() {
     // Cadastro Carta 1:
-    char Estado;
-    char CodigoCarta[30];
-    char Cidade[30];
-    int Populacao;
-    float Areakm2;
-    float PIB;
-    int PontosTuristicos;
+    char Estado; //estado: A
+    char CodigoCarta[30]; //codigo carta:A01
+    char Cidade[30]; //cidade: Aracaju
+    int Populacao;//populacao:602.757
+    float Areakm2;//area: 182,163
+    float PIB;//pib: 16.447 bilhoes
+    int PontosTuristicos; //pontos turisticos: 32
+    float Densidadepopulacional; // Populacao/Areakm2
+    float PIBpercapita; // PIB/Populacao
+    
     
     //Solicita ao usuario que insira o nome do Estado da primeira carta
     printf("Estado: \n");
@@ -41,25 +44,38 @@ int main() {
     printf("Pontos Turisticos: \n");
     scanf("%d", &PontosTuristicos);
 
-    //Solicita que o usuario coloque as informações necessarias da primeira carta
-    printf("Carta 1: \n");
+    //Operação aritmetica realizada para calcular a densidade populacional da primeira carta
+    Densidadepopulacional = Populacao / Areakm2;
+
+    //Operação aritmetica realizada para calcular o PIB per Capita da primeira carta
+    PIBpercapita = (PIB * 1000000000) / Populacao;
+
+
+    //Impressão da sinformações colocadas pelo usuario
+    printf("*** Cadastro Carta 1 ***\n");
     printf("Estado Carta 1: %c\n", Estado);
     printf("Codigo da Carta 1: %s\n", CodigoCarta);
     printf("Nome da Cidade Carta 1: %s\n", Cidade);
     printf("População Carta 1: %d\n", Populacao);
-    printf("Area em km² Carta 1: %f\n", Areakm2);
-    printf("PIB Carta 1: %f\n", PIB);
+    printf("Area em km² Carta 1: %f km²\n", Areakm2);
+    printf("PIB Carta 1: %.2f bilhões de reais\n", PIB);
     printf("Pontos Turisticos Carta 1: %d\n", PontosTuristicos);
+    printf("Densidade Populacional Carta 1: %.2f hab/km²\n", Densidadepopulacional);
+    printf("PIB per capita Carta 1: %.2f reais\n", PIBpercapita);
+   
 
 
     // Cadastro Carta 2:
-    char estado;
-    char codigoCarta[30];
-    char cidade[30];
-    int populacao;
-    float areakm2;
-    float pib;
-    int pontosTuristicos;
+    char estado; //estado:B
+    char codigoCarta[30]; //codigo carta:B01
+    char cidade[30]; // cidade:Recife
+    int populacao; //população:1.488.920
+    float areakm2; //area:218,843
+    float pib; // pib:54.970 milhões
+    int pontosTuristicos; //pontos turisticos:50
+    float densidadepopulacional; // populacao/areakm2
+    float pibpercapita; // pib/populacao
+  
     
     //Solicita ao usuario que insira o nome do Estado da segunda carta
     printf("Estado: \n");
@@ -89,15 +105,29 @@ int main() {
     printf("Pontos Turisticos: \n");
     scanf("%d", &pontosTuristicos);
 
+     //Operação aritmetica realizada para calcular a densidade populacional da primeira carta
+  
+     densidadepopulacional = populacao / areakm2;
+
+ 
+     //Operação aritmetica realizada para calcular o PIB per Capita da primeira carta
+    
+    pibpercapita = (pib * 1000000000) / populacao;
+
+
+
     //Solicita que o usuario coloque as informações necessarias da segunda carta
-    printf("Carta 2: \n");
+    printf("*** Cadastro Carta 2 ***\n");
     printf("Estado Carta 2: %c\n", estado);
     printf("Codigo da Carta 2: %s\n", codigoCarta);
     printf("Nome da Cidade Carta 2: %s\n", cidade);
     printf("População Carta 2: %d\n", populacao);
-    printf("Area em km² Carta 2: %f\n", areakm2);
-    printf("PIB Carta 2: %f\n", pib);
+    printf("Area em km² Carta 2: %f km²\n", areakm2);
+    printf("PIB Carta 2: %.2f milhões de reais\n", pib);
     printf("Pontos Turisticos Carta 2: %d\n", pontosTuristicos);
+    printf("Densidade Populacional Carta 2: %.2f hab/km²\n", densidadepopulacional);
+    printf("PIB per capita Carta 2: %.2f reais\n", pibpercapita);
+
 
     return 0;
 }
